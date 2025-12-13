391b3240515a476e411414f4e99abcf3b01115fa
Kono <wedoza112@gmail.com>
2025-12-13 11:54:39 +0700

    Add files via upload

---
+++ 3-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Product
+{
+    char name[50];
+    int productid;
+    float price;
+};
+
+int main(){ 
+    
+    struct Product item;
+
+    if (scanf("%d %f %s", &item.productid, &item.price, item.name) != 3)
+    {
+        return 1;
+    }
+    
+    printf("ID: %d, Price: %.2f, Name: %s\n", item.productid, item.price, item.name);
+
+    return 0;
+    
+}
